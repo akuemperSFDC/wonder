fb3c44f386b008673ca069b8ef4be2041159eb72
Austin Kuemper <78562233+AKuemper@users.noreply.github.com>
2021-08-23 13:22:30 -0700

    added comment seeds, successfully seeded db

--- backend/db/seeders/20210823201827-comments-seeds.js
+++ backend/db/seeders/20210823201827-comments-seeds.js
@@ -7,11 +7,41 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+      */
+    return queryInterface.bulkInsert(
+      'Comments',
+      [
+        {
+          answerId: 1,
+          ownerId: 1,
+          content: 'Super cool!',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          answerId: 1,
+          ownerId: 3,
+          content: 'Nice!',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          answerId: 1,
+          ownerId: 3,
+          content: 'Awesome!',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          answerId: 1,
+          ownerId: 2,
+          content: 'Neat!',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
@@ -20,7 +50,7 @@ module.exports = {
       Return a promise to correctly handle asynchronicity.
 
       Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+      */
+    return queryInterface.bulkDelete('Comments', null, {});
+  },
 };
